1fd7e680c8e9c1c6af0a04436a18d96a38860e2b
AbdulRaheem Olurode <abdraheemolu2020@gmail.com>
2026-08-15 03:37:18 +0100

    queries modified

--- apps/web/src/lib/queries.ts
+++ apps/web/src/lib/queries.ts
@@ -1,15 +1,18 @@
-import {sanityClient} from './sanity'
-
-export async function getArticles() {
-  return sanityClient.fetch(`
-    *[_type == "article"] | order(publishedAt desc) {
-      _id,
-      title,
-      "slug": slug.current,
-      excerpt,
-      publishedAt,
-      contentType,
-      featured
-    }
-  `)
+export async function getArticle(slug: string) {
+  return sanityClient.fetch(
+    `
+      *[_type == "article" && slug.current == $slug][0] {
+        _id,
+        title,
+        "slug": slug.current,
+        excerpt,
+        publishedAt,
+        contentType,
+        featured,
+        featuredImage,
+        body
+      }
+    `,
+    {slug},
+  )
 }
