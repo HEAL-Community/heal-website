dc0d8b31462eac06d00097efd1cbfbdc38b24c23
AbdulRaheem Olurode <abdraheemolu2020@gmail.com>
2026-08-30 19:06:18 +0100

    Revert "Update layout.tsx"
    This reverts commit 936c84653fab22f195001cd920d3b787591fbb69.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 
-import SiteHeader from "@/components/SiteHeader";
-import SiteFooter from "@/components/SiteFooter";
-
 const manrope = Manrope({
   variable: "--font-manrope",
   subsets: ["latin"],
@@ -108,13 +105,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${manrope.variable} antialiased`}>
-        <SiteHeader />
-
-        {children}
-
-        <SiteFooter />
-      </body>
+      <body className={`${manrope.variable} antialiased`}>{children}</body>
     </html>
   );
 }
