936c84653fab22f195001cd920d3b787591fbb69
AbdulRaheem Olurode <abdraheemolu2020@gmail.com>
2026-08-30 18:55:25 +0100

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 
+import SiteHeader from "@/components/SiteHeader";
+import SiteFooter from "@/components/SiteFooter";
+
 const manrope = Manrope({
   variable: "--font-manrope",
   subsets: ["latin"],
@@ -105,7 +108,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${manrope.variable} antialiased`}>{children}</body>
+      <body className={`${manrope.variable} antialiased`}>
+        <SiteHeader />
+
+        {children}
+
+        <SiteFooter />
+      </body>
     </html>
   );
 }
